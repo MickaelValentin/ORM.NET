c50e63e402a768b8dd43227907c2971245347b99
ThomasBerranger <tberranger@hotmail.fr>
2017-11-16 20:01:19 +0100

    Base - sql connection - read - insert in progress

---
+++ ConsoleAppBach3/Program.cs
@@ -0,0 +1,248 @@
+﻿using ConsoleAppBach3.Metier;
+using ConsoleAppBach3.Metier.Comparateur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppBach3.Outils;
+using System.Xml.Linq;
+using Project_Orn;
+
+
+
+namespace ConsoleAppBach3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nA quelle base de donnée souhaitez-vous vous connecter ?");
+                Console.WriteLine(" 1 - SQL \n 2 - Oracle \n 3 - Postgre \n q - Quitter");
+
+                string choix = Console.ReadLine();
+                switch (choix.ToLower())
+                {
+                    case "1":
+                        Console.WriteLine("SQL");
+                        Orm.testSqlServer();
+                        break;
+                    case "2":
+                        Console.WriteLine("ORACLE");
+                        break;
+                    case "3":
+                        Console.WriteLine("POSTGRE");
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        Console.WriteLine("Erreur dans le choix");
+                        break;
+                } 
+            }
+        }
+
+
+
+#region cours
+        private static void TPLinq()
+        {
+            Console.WriteLine("Date début ?");
+            DateTime debut = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Date fin ?");
+            DateTime fin = Convert.ToDateTime(Console.ReadLine());
+
+            List<Contact> list = Contact.Lister();
+            XDocument doc = XDocument.Load(@"c:\contacts.xml");
+            IEnumerable<XElement> elements = doc.Root.Elements();
+
+            var resultat = from c in list
+                           join e in elements
+                           on c.Mail equals e.Attribute("email").Value
+                           where c.DateNaissance >= debut && c.DateNaissance <= fin
+                           orderby c.Nom
+                           select new
+                           {
+                               c.Prenom,
+                               c.Nom,
+                               Rue = e.Element("Adresse").Element("Rue").Value,
+                               CodePostal = e.Element("Adresse").Element("Cp").Value,
+                               Ville = e.Element("Adresse").Element("Ville").Value
+                           };
+
+            var resultatBis = list.Where(c => c.DateNaissance >= debut && c.DateNaissance <= fin)
+                                    .Join(elements,
+                                        c => c.Mail,
+                                        e => e.Attribute("email").Value,
+                                        (c, e) => new
+                                        {
+                                            c.Prenom,
+                                            c.Nom,
+                                            Rue = e.Element("Adresse").Element("Rue").Value,
+                                            CodePostal = e.Element("Adresse").Element("Cp").Value,
+                                            Ville = e.Element("Adresse").Element("Ville").Value
+                                        });
+
+            foreach (var item in resultat)
+            {
+                Console.WriteLine(item.Nom + " " + item.Ville);
+            }
+
+        }
+
+        private static void linq()
+        {
+
+            List<Contact> list = Contact.Lister();
+            /*list.OrderBy(delegate (Contact c)
+            {
+                return c.Prenom;
+            });*/
+            var resultat = list.Where(c => c.Nom.Contains("T")).OrderBy(c => c.Prenom);
+
+            var resultatBis = from c in list
+                              where c.Nom.Contains("T")
+                              orderby c.Prenom
+                              select c;
+        }
+
+        private static void rechercheContactParDate()
+        {
+            Console.WriteLine("Date début ?");
+            DateTime debut = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Date fin ?");
+            DateTime fin = Convert.ToDateTime(Console.ReadLine());
+
+            List<Contact> list = Contact.Lister();
+            //List<Contact> resultat = list.FindAll(methodeRechercheParDate);
+            List<Contact> resultat = list.FindAll(delegate (Contact c)
+            {
+                return c.DateNaissance >= debut && c.DateNaissance <= fin;
+            });
+
+            foreach (var item in resultat)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        /*private static bool methodeRechercheParDate(Contact c)
+        {
+            return c.DateNaissance >= debut && c.DateNaissance <= fin;
+        }*/
+
+        private static void rechercheContactParNom()
+        {
+            List<Contact> list = Contact.Lister();
+            List<Contact> resultat = list.FindAll(methodeDeRecherche);
+            foreach (var item in resultat)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        private static bool methodeDeRecherche(Contact c)
+        {
+            return c.Nom.Contains("T");
+        }
+
+        private static void extension()
+        {            
+            DateTime dt = DateTime.Now;
+            //Extension.PremierJourSemaine(dt);
+            Console.WriteLine(  dt.PremierJourSemaine());
+        }
+
+        private static void triGeneric()
+        {
+            List<Contact> list = Contact.Lister();
+            ComparateurGeneric<Contact> comp = new ComparateurGeneric<Contact>("Mail", "DateNaissance");
+            list.Sort(comp);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        private static void trierContactsParDate()
+        {
+            List<Contact> list = Contact.Lister();
+            ComparateurContactDate comp = new ComparateurContactDate();
+            list.Sort(comp);
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Prenom} {item.Nom} - {item.DateNaissance}");
+            }
+        }
+
+        private static void trierContacts()
+        {
+            List<Contact> list = Contact.Lister();
+            list.Sort();
+            
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Prenom} {item.Nom}");
+            }
+        }
+        
+        private static void afficherContact()
+        {
+            List<Contact> list = Contact.Lister();
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Prenom} {item.Nom}");
+            }
+        }
+
+        private static void ajouterContact()
+        {
+            Contact c = new Contact();
+            Console.WriteLine("Nom ?");
+            c.Nom = Console.ReadLine();
+
+            Console.WriteLine("Prénom ?");
+            c.Prenom = Console.ReadLine();
+
+            Console.WriteLine("Mail ?");
+            c.Mail = Console.ReadLine();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Date de naissance ?");
+                    c.DateNaissance = Convert.ToDateTime(Console.ReadLine());
+                    break;
+                }catch(Exception e)
+                {
+                    Console.WriteLine($"Erreur : {e.Message}");
+                }
+            }
+            c.Enregistrer();
+            //Console.WriteLine(c.ToString());
+        }
+
+        private static void afficherMenu()
+        {
+            Console.WriteLine("-- MENU --");
+            Console.WriteLine("1- Ajouter un contact");
+            Console.WriteLine("2- Afficher les contacts");
+            Console.WriteLine("3- Trier les contacts");
+            Console.WriteLine("4- Trier les contacts par date");
+            Console.WriteLine("5- Tri généric");
+            Console.WriteLine("6- Méthode d'extension");
+            Console.WriteLine("7- Recherche par nom");
+            Console.WriteLine("8- Recherche par dates");
+            Console.WriteLine("9- Linq");
+            Console.WriteLine("10- TPLinq");
+            Console.WriteLine("Q- Quitter");
+        }
+    }
+}
+#endregion
---
+++ Project_Orn/Orm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Project_Orn
+{
+    public  static class Orm
+    {
+
+        public static void testSqlServer()
+        {
+            // Instantiate the connection
+            SqlConnection conn = new SqlConnection("Data Source=(local);Initial Catalog=orm;User ID=thomas;Password=thomas");
+    
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // Informe the user if it's a succed
+                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insrer une valeure");
+
+                string choix = Console.ReadLine();
+                switch (choix.ToLower())
+                {
+                    case "1":
+                        Console.WriteLine("Affichage complet :");
+                        
+                        // Pass the connection to a command object
+                        SqlCommand cmd = new SqlCommand("select * from ormTable", conn);
+
+                        //
+                        // Use the connection
+                        //
+
+                        // get query results
+                        rdr = cmd.ExecuteReader();
+
+                        // print the CustomerID of each record
+                        while (rdr.Read())
+                        {
+                            Console.WriteLine(rdr[1]);
+                        }
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Insérer");
+                        break;
+                    default:
+                        Console.WriteLine("Erreur dans le choix");
+                        break;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erreur : {e.Message}");
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // 5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+        /*
+        public static void insertSqlServer()
+        {
+            Console.WriteLine("INSERT :");
+
+            SqlConnection conn = new SqlConnection( "Data Source=(local);Initial Catalog=orm;User ID=thomas;Password=thomas");
+            SqlDataReader rdr = null;
+            conn.Open();
+
+
+
+            string insertString = @"insert into ormTable (id, name) values ('', 'test')";
+            
+
+
+            // 3. Pass the connection to a command object
+            SqlCommand cmd = new SqlCommand("insert into ormTable", conn);
+
+
+            // 2. Call ExecuteNonQuery to send command
+            cmd.ExecuteNonQuery();
+
+            Console.Read();
+        }
+
+        public static void testOracle()
+        {
+            OdbcConnection conn = new OdbcConnection("DSN=SAMPLE_ISAM");
+
+ 
+            // 1. Instantiate the connection
+          
+
+
+            OdbcDataReader rdr = null;
+
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                // 3. Pass the connection to a command object
+                OdbcCommand cmd = new OdbcCommand("select * from Customerss", conn);
+
+                //
+                // 4. Use the connection
+                //
+
+                // get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CustomerID of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // 5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }*/
+    }
+}
+
