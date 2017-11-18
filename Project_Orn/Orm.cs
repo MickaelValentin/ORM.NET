bf763bf1242b5ae9360283644c30a5a4ac2a4ca5
ThomasBerranger <tberranger@hotmail.fr>
2017-11-18 16:36:42 +0100

    Insert done - Update done - Base modified - Code optimized

--- Project_Orn/Orm.cs
+++ Project_Orn/Orm.cs
@@ -25,7 +25,7 @@ namespace Project_Orn
                 conn.Open();
 
                 // Informe the user if it's a succed
-                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insrer une valeure");
+                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insérer une valeure \n 3 - Modifier une valeure");
 
                 string choix = Console.ReadLine();
                 switch (choix.ToLower())
@@ -35,10 +35,6 @@ namespace Project_Orn
                         
                         // Pass the connection to a command object
                         SqlCommand cmd = new SqlCommand("select * from ormTable", conn);
-
-                        //
-                        // Use the connection
-                        //
 
                         // get query results
                         rdr = cmd.ExecuteReader();
@@ -52,6 +48,11 @@ namespace Project_Orn
                         break;
                     case "2":
                         Console.WriteLine("Insérer");
+                        insertSqlServer(conn);
+                        break;
+                    case "3":
+                        Console.WriteLine("Modifier");
+                        updateSqlServer(conn);
                         break;
                     default:
                         Console.WriteLine("Erreur dans le choix");
@@ -66,13 +67,13 @@ namespace Project_Orn
             }
             finally
             {
-                // close the reader
+                // Close the reader
                 if (rdr != null)
                 {
                     rdr.Close();
                 }
 
-                // 5. Close the connection
+                // Close the connection
                 if (conn != null)
                 {
                     conn.Close();
@@ -80,32 +81,49 @@ namespace Project_Orn
             }
         }
 
-
-        /*
-        public static void insertSqlServer()
+        
+        public static void insertSqlServer(SqlConnection conn)
         {
             Console.WriteLine("INSERT :");
+            Console.WriteLine("Quelle nom voulez-vous insérer ?");
+            String name = Console.ReadLine();
 
-            SqlConnection conn = new SqlConnection( "Data Source=(local);Initial Catalog=orm;User ID=thomas;Password=thomas");
-            SqlDataReader rdr = null;
-            conn.Open();
+            string insertString = @"insert into ormTable (name) values (@name)";
 
+            SqlCommand cmd = new SqlCommand(insertString, conn);
 
+            cmd.Parameters.AddWithValue("@name", name);
 
-            string insertString = @"insert into ormTable (id, name) values ('', 'test')";
-            
-
-
-            // 3. Pass the connection to a command object
-            SqlCommand cmd = new SqlCommand("insert into ormTable", conn);
-
-
-            // 2. Call ExecuteNonQuery to send command
             cmd.ExecuteNonQuery();
 
             Console.Read();
         }
 
+
+
+        public static void updateSqlServer(SqlConnection conn)
+        {
+            Console.WriteLine("UPDATE :");
+            Console.WriteLine("Quelle nom voulez-vous mofidier ?");
+            String currentName = Console.ReadLine();
+            Console.WriteLine("Que rentrer à la place ?");
+            String newName = Console.ReadLine();
+
+            string updateString = @" update ormTable set name = @newName where name = @currentName";
+
+            SqlCommand cmd = new SqlCommand(updateString);
+
+            cmd.Parameters.AddWithValue("@currentName", currentName);
+            cmd.Parameters.AddWithValue("@newName", newName);
+
+            cmd.Connection = conn;
+            
+            cmd.ExecuteNonQuery();
+        }
+
+
+
+        /*
         public static void testOracle()
         {
             OdbcConnection conn = new OdbcConnection("DSN=SAMPLE_ISAM");
