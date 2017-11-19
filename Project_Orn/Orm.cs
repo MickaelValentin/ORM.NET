e465ee64f754f3c467d18cf17e3b2c68226b53ef
ThomasBerranger <tberranger@hotmail.fr>
2017-11-19 15:38:31 +0100

    Delete done

--- Project_Orn/Orm.cs
+++ Project_Orn/Orm.cs
@@ -25,7 +25,7 @@ namespace Project_Orn
                 conn.Open();
 
                 // Informe the user if it's a succed
-                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insérer une valeure \n 3 - Modifier une valeure");
+                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insérer une valeure \n 3 - Modifier une valeure \n 4 - Supprimer une valeure");
 
                 string choix = Console.ReadLine();
                 switch (choix.ToLower())
@@ -53,6 +53,10 @@ namespace Project_Orn
                     case "3":
                         Console.WriteLine("Modifier");
                         updateSqlServer(conn);
+                        break;
+                    case "4":
+                        Console.WriteLine("Supprimer");
+                        deleteSqlServer(conn);
                         break;
                     default:
                         Console.WriteLine("Erreur dans le choix");
@@ -100,7 +104,6 @@ namespace Project_Orn
         }
 
 
-
         public static void updateSqlServer(SqlConnection conn)
         {
             Console.WriteLine("UPDATE :");
@@ -121,56 +124,75 @@ namespace Project_Orn
             cmd.ExecuteNonQuery();
         }
 
-
-
-        /*
-        public static void testOracle()
+        public static void deleteSqlServer(SqlConnection conn)
         {
-            OdbcConnection conn = new OdbcConnection("DSN=SAMPLE_ISAM");
+            Console.WriteLine("DELETE :");
+            Console.WriteLine("Quelle nom voulez-vous supprimer ?");
+            String deleteName = Console.ReadLine();
 
- 
-            // 1. Instantiate the connection
-          
+            string deleteString = @" delete from ormTable where name = @deleteName";
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Parameters.AddWithValue("@deleteName", deleteName);
+
+            cmd.CommandText = deleteString;
+            
+            cmd.Connection = conn;
+            
+            cmd.ExecuteNonQuery();
+
+        }
 
 
-            OdbcDataReader rdr = null;
-
-            try
+            /*
+            public static void testOracle()
             {
-                // 2. Open the connection
-                conn.Open();
+                OdbcConnection conn = new OdbcConnection("DSN=SAMPLE_ISAM");
 
-                // 3. Pass the connection to a command object
-                OdbcCommand cmd = new OdbcCommand("select * from Customerss", conn);
 
-                //
-                // 4. Use the connection
-                //
+                // 1. Instantiate the connection
 
-                // get query results
-                rdr = cmd.ExecuteReader();
 
-                // print the CustomerID of each record
-                while (rdr.Read())
+
+                OdbcDataReader rdr = null;
+
+                try
                 {
-                    Console.WriteLine(rdr);
-                }
-            }
-            finally
-            {
-                // close the reader
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
+                    // 2. Open the connection
+                    conn.Open();
 
-                // 5. Close the connection
-                if (conn != null)
-                {
-                    conn.Close();
+                    // 3. Pass the connection to a command object
+                    OdbcCommand cmd = new OdbcCommand("select * from Customerss", conn);
+
+                    //
+                    // 4. Use the connection
+                    //
+
+                    // get query results
+                    rdr = cmd.ExecuteReader();
+
+                    // print the CustomerID of each record
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine(rdr);
+                    }
                 }
-            }
-        }*/
-    }
+                finally
+                {
+                    // close the reader
+                    if (rdr != null)
+                    {
+                        rdr.Close();
+                    }
+
+                    // 5. Close the connection
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+            }*/
+        }
 }
 
