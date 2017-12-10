b2b3524308e0914a64163a0ac5d03345c1eec262
MickaelValentin <mickael.valentin@ynov.com>
2017-12-10 17:42:13 +0100

    Merge library

---
+++ ConsoleAppBach3/Project_Orn/MySQL.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Orn
+{
+    class MySQL
+    {
+        public static void createSqlTable()
+        {
+            OdbcConnection conn = new OdbcConnection(
+
+
+            "DRIVER={MySQL ODBC 5.3 ANSI Driver};" +
+                   "SERVER=localhost;" +
+                   "DATABASE=orm;" +
+                   "USER=root;");
+
+            Console.WriteLine("Créer ->");
+            Console.WriteLine("Enter your table name");
+            string nomTable = Console.ReadLine();
+            Console.WriteLine($"Entrer le nombre de colonnes de la table {nomTable}");
+            string nombreColonne = Console.ReadLine();
+            int nombre = Int32.Parse(nombreColonne);
+            string nomCol;
+            string nomAllCol = "";
+            string type;
+            string taille;
+
+            for (int i = 0; i < nombre; i++)
+            {
+                Console.WriteLine($"Colonne {i + 1} : ");
+                nomCol = Console.ReadLine();
+
+                Console.WriteLine($"Type de la colonne {nomCol} : ");
+                type = Console.ReadLine();
+
+                Console.WriteLine($"Taille/Valeur : ");
+                taille = Console.ReadLine();
+
+                type.ToLower();
+
+                if (i == 0)
+                {
+                    nomAllCol = $"{nomCol} {type}({taille})";
+                }
+                else
+                {
+                    nomAllCol = $"{nomAllCol},{nomCol} {type}({taille})";
+                }
+
+            }
+
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                //OdbcCommand cmd = new OdbcCommand($"CREATE TABLE {nomTable}()", conn);
+                OdbcCommand cmd = new OdbcCommand($"CREATE TABLE {nomTable} ({nomAllCol})", conn);
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("La table à bien été créer");
+            }
+
+            finally
+            {
+
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public static void selectTable()
+        {
+            OdbcConnection conn = new OdbcConnection(
+
+
+           "DRIVER={MySQL ODBC 5.3 ANSI Driver};" +
+                  "SERVER=localhost;" +
+                  "DATABASE=orm;" +
+                  "USER=root;");
+
+            Console.WriteLine("Rechercher ->");
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                Console.WriteLine("Table : ?");
+                string nomTable = Console.ReadLine();
+
+                Console.WriteLine("Colonne : ? (* pour tout lister)");
+                string nomColonne = Console.ReadLine();
+
+                //OdbcCommand cmd = new OdbcCommand($"CREATE TABLE {nomTable}()", conn);
+                OdbcCommand cmd = new OdbcCommand($"SELECT {nomColonne} FROM {nomTable}", conn);
+                OdbcDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[1]);
+
+                }
+            }
+
+            finally
+            {
+
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public static void dropTable()
+        {
+            OdbcConnection conn = new OdbcConnection(
+
+
+           "DRIVER={MySQL ODBC 5.3 ANSI Driver};" +
+                  "SERVER=localhost;" +
+                  "DATABASE=orm;" +
+                  "USER=root;");
+
+            Console.WriteLine("Supprimer ->");
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                Console.WriteLine("1 - Supprimer une table");
+                Console.WriteLine("2 - Supprimer une colonne");
+
+                string switchString = Console.ReadLine();
+                int caseSwitch = Int32.Parse(switchString);
+
+                Console.WriteLine("Table :  ?");
+                string nomTable = Console.ReadLine();
+
+                switch (caseSwitch)
+                {
+                    case 1:
+                        OdbcCommand cmdTable = new OdbcCommand($"DROP TABLE IF EXISTS {nomTable}", conn);
+                        cmdTable.ExecuteNonQuery();
+                        break;
+                    case 2:
+                        Console.WriteLine("Colonne : ?");
+                        string nomColonne = Console.ReadLine();
+
+                        OdbcCommand cmdCol = new OdbcCommand($"ALTER TABLE {nomTable} DROP COLUMN {nomColonne}", conn);
+                        cmdCol.ExecuteNonQuery();
+                        break;
+                    default:
+                        Console.WriteLine("Une erreur est survenu");
+                        break;
+                }
+
+
+
+
+                //cmd.ExecuteNonQuery();
+
+                Console.WriteLine($"La table {nomTable} à bien été suprimer");
+                Console.WriteLine("Appuyer sur une touche pour continuer");
+                Console.ReadLine();
+            }
+
+            finally
+            {
+
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public static void insert()
+        {
+            OdbcConnection conn = new OdbcConnection(
+
+
+           "DRIVER={MySQL ODBC 5.3 ANSI Driver};" +
+                  "SERVER=localhost;" +
+                  "DATABASE=orm;" +
+                  "USER=root;");
+
+            Console.WriteLine("Insérer ->");
+
+            string nomTable;
+            string nomColonne;
+            string valeur;
+            string nombreColonne;
+            int nombre;
+            string allColonne = "";
+            string allValeur = "";
+
+            try
+            {
+                conn.Open();
+
+                Console.WriteLine("Table : ?");
+                nomTable = Console.ReadLine();
+
+                Console.WriteLine("Nombre colonne : ?");
+                nombreColonne = Console.ReadLine();
+                nombre = Int32.Parse(nombreColonne);
+
+                for (int i = 0; i < nombre; i++)
+                {
+                    Console.WriteLine($"Nom colonne {i + 1} : ?");
+                    nomColonne = Console.ReadLine();
+
+                    Console.WriteLine($"Valeur colonne {i + 1} : ?");
+                    valeur = Console.ReadLine();
+
+                    if (i == 0)
+                    {
+                        allColonne = nomColonne;
+                        allValeur = valeur;
+                    }
+                    else
+                    {
+                        allColonne = $"{allColonne}, {nomColonne}";
+                        allValeur = $"{allValeur}, {valeur}";
+                    }
+
+
+                }
+
+                OdbcCommand cmd = new OdbcCommand($"INSERT INTO {nomTable} ({allColonne}) VALUES ({allValeur})", conn);
+                cmd.ExecuteReader();
+
+
+            }
+
+            finally
+            {
+
+
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppBach3/Project_Orn/SQLServer.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Orn
+{
+    class SQLServer
+    {
+        public void connection()
+        {
+            // Instantiate the connection
+            SqlConnection conn = new SqlConnection("Data Source=(local);Initial Catalog=orm;User ID=thomas;Password=thomas");
+
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // Informe the user if it's a succed
+                Console.WriteLine("Connection établie, que souhaitez-vous faire ? \n 1 - Lire la base de donnée \n 2 - Insérer une valeure \n 3 - Modifier une valeure \n 4 - Supprimer une valeure\n 5 - Créer une table\n 6 - Suppimer une table");
+
+                string choix = Console.ReadLine();
+                switch (choix.ToLower())
+                {
+                    case "1":
+                        readSqlServer(conn, rdr);
+                        break;
+                    case "2":
+                        insertSqlServer(conn);
+                        break;
+                    case "3":
+                        updateSqlServer(conn);
+                        break;
+                    case "4":
+                        deleteSqlServer(conn);
+                        break;
+                    case "5":
+                        createTableSqlServer(conn);
+                        break;
+                    case "6":
+                        deleteTableSqlServer(conn);
+                        break;
+                    default:
+                        Console.WriteLine("Erreur dans le choix");
+                        break;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erreur : {e.Message}");
+            }
+            finally
+            {
+                // Close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void createTableSqlServer(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText =
+                @" 
+                        BEGIN
+                        CREATE TABLE ormTableCreated (
+                          Id    integer PRIMARY KEY NOT NULL,
+                          Name  varchar(200) NOT NULL
+                        ); 
+                    END";
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Table créée");
+        }
+
+        private void deleteTableSqlServer(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText =
+                @" 
+                    BEGIN
+                    DROP TABLE ormTableCreated; 
+                END";
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Table supprimée");
+        }
+
+        private static void readSqlServer(SqlConnection conn, SqlDataReader rdr)
+        {
+            // Pass the connection to a command object
+            SqlCommand cmd = new SqlCommand("select * from ormTable", conn);
+
+            // get query results
+            rdr = cmd.ExecuteReader();
+
+            // print the CustomerID of each record
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[1]);
+            }
+
+        }
+
+        public static void insertSqlServer(SqlConnection conn)
+        {
+            Console.WriteLine("INSERT :");
+            Console.WriteLine("Quelle nom voulez-vous insérer ?");
+            String name = Console.ReadLine();
+
+            string insertString = @"insert into ormTable (name) values (@name)";
+
+            SqlCommand cmd = new SqlCommand(insertString, conn);
+
+            cmd.Parameters.AddWithValue("@name", name);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void updateSqlServer(SqlConnection conn)
+        {
+            Console.WriteLine("UPDATE :");
+            Console.WriteLine("Quelle nom voulez-vous mofidier ?");
+            String currentName = Console.ReadLine();
+            Console.WriteLine("Que rentrer à la place ?");
+            String newName = Console.ReadLine();
+
+            string updateString = @" update ormTable set name = @newName where name = @currentName";
+
+            SqlCommand cmd = new SqlCommand(updateString);
+
+            cmd.Parameters.AddWithValue("@currentName", currentName);
+            cmd.Parameters.AddWithValue("@newName", newName);
+
+            cmd.Connection = conn;
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void deleteSqlServer(SqlConnection conn)
+        {
+            Console.WriteLine("DELETE :");
+            Console.WriteLine("Quelle nom voulez-vous supprimer ?");
+            String deleteName = Console.ReadLine();
+
+            string deleteString = @" delete from ormTable where name = @deleteName";
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Parameters.AddWithValue("@deleteName", deleteName);
+
+            cmd.CommandText = deleteString;
+
+            cmd.Connection = conn;
+
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
